b6e0b028e56807c3dd76be5c470ada5c5a89b75b
Roman <impyls95@mail.ru>
2024-04-14 23:28:12 +0300

    refactor hw_1

--- lesson1/test_main.py
+++ lesson1/test_main.py
@@ -30,97 +30,98 @@ def test_auth_negative(driver):
 
 # Корзина
 def test_add_product_to_cart_from_catalog(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.XPATH, locators.add_backpack).click()
-    assert driver.find_element(By.XPATH, locators.cart_badge).text == '1'
-    driver.find_element(By.CLASS_NAME, locators.cart_link).click()
-    assert driver.find_element(By.CLASS_NAME, locators.cart_quantity).text == '1'
+    browser.find_element(By.XPATH, locators.add_backpack).click()
+    assert browser.find_element(By.XPATH, locators.cart_badge).text == '1'
+    browser.find_element(By.CLASS_NAME, locators.cart_link).click()
+    assert browser.find_element(By.CLASS_NAME, locators.cart_quantity).text == '1'
 
 
 def test_remove_product_from_cart_in_cart(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.XPATH, locators.add_backpack).click()
-    assert driver.find_element(By.XPATH, locators.cart_badge).text == '1'
-    driver.find_element(By.CLASS_NAME, locators.cart_link).click()
-    assert driver.find_element(By.CLASS_NAME, locators.cart_quantity).text == '1'
+    browser.find_element(By.XPATH, locators.add_backpack).click()
+    assert browser.find_element(By.XPATH, locators.cart_badge).text == '1'
+    browser.find_element(By.CLASS_NAME, locators.cart_link).click()
+    assert browser.find_element(By.CLASS_NAME, locators.cart_quantity).text == '1'
 
-    driver.find_element(By.XPATH, locators.remove_backpack).click()
-    #  assert driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span').is_displayed()
+    browser.find_element(By.XPATH, locators.remove_backpack).click()
+    #  assert browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span').is_displayed()
     with pytest.raises(NoSuchElementException):
-        driver.find_element(By.XPATH, locators.cart_badge)
+        browser.find_element(By.XPATH, locators.cart_badge)
         pytest.fail('Не должно быть товара в корзине')
 
 
 def test_add_product_in_cart_from_products_cards(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.XPATH, locators.item_0_link).click()
-    driver.find_element(By.CSS_SELECTOR, locators.add_to_cart).click()
-    assert driver.find_element(By.XPATH, locators.cart_badge).text == '1'
-    driver.find_element(By.CLASS_NAME, locators.cart_link).click()
-    assert driver.find_element(By.CLASS_NAME, locators.cart_quantity).text == '1'
+    browser.find_element(By.XPATH, locators.item_0_link).click()
+    browser.find_element(By.CSS_SELECTOR, locators.add_to_cart).click()
+    assert browser.find_element(By.XPATH, locators.cart_badge).text == '1'
+    browser.find_element(By.CLASS_NAME, locators.cart_link).click()
+    assert browser.find_element(By.CLASS_NAME, locators.cart_quantity).text == '1'
 
 
 def test_remove_product_from_cart_in_products_cards(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.XPATH, locators.item_0_link).click()
-    driver.find_element(By.CSS_SELECTOR, locators.add_to_cart).click()
-    assert driver.find_element(By.XPATH, locators.cart_badge).text == '1'
-    driver.find_element(By.XPATH, locators.remove_from_prod_cart).click()
-    driver.find_element(By.CLASS_NAME, locators.cart_link).click()
+    browser.find_element(By.XPATH, locators.item_0_link).click()
+    browser.find_element(By.CSS_SELECTOR, locators.add_to_cart).click()
+    assert browser.find_element(By.XPATH, locators.cart_badge).text == '1'
+    browser.find_element(By.XPATH, locators.remove_from_prod_cart).click()
+    browser.find_element(By.CLASS_NAME, locators.cart_link).click()
     with pytest.raises(NoSuchElementException):
-        driver.find_element(By.XPATH, locators.cart_badge)
+        browser.find_element(By.XPATH, locators.cart_badge)
         pytest.fail('Не должно быть товаров в корзине')
 
 
 # Карточка товара
 def test_successful_open_product_card_after_click_om_image_prod(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.CSS_SELECTOR, locators.item_1_img_link).click()
-    assert driver.current_url == 'https://www.saucedemo.com/inventory-item.html?id=1'
+    browser.find_element(By.CSS_SELECTOR, locators.item_1_img_link).click()
+    assert browser.current_url == 'https://www.saucedemo.com/inventory-item.html?id=1'
 
 
 def test_successful_open_product_card_after_click_on_title_prod(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.CSS_SELECTOR, locators.item_5_img_link).click()
-    assert driver.current_url == 'https://www.saucedemo.com/inventory-item.html?id=5'
+    browser.find_element(By.CSS_SELECTOR, locators.item_5_img_link).click()
+    assert browser.current_url == 'https://www.saucedemo.com/inventory-item.html?id=5'
 
 
 # Оформление заказа
 def test_placing_an_order_positive(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.XPATH, locators.add_to_cart_red_tshirt).click()
-    assert driver.find_element(By.XPATH, locators.cart_badge).text == '1'
-    driver.find_element(By.CLASS_NAME, locators.cart_link).click()
-    assert driver.find_element(By.CLASS_NAME, locators.cart_quantity).text == '1'
-    driver.find_element(By.CSS_SELECTOR, locators.checkout).click()
-    assert driver.current_url == 'https://www.saucedemo.com/checkout-step-one.html'
-    driver.find_element(By.CSS_SELECTOR, locators.first_name_field).send_keys('Howard')
-    driver.find_element(By.CSS_SELECTOR, locators.last_name_field).send_keys('Lovecraft')
-    driver.find_element(By.CSS_SELECTOR, locators.postal_code).send_keys('322')
-    driver.find_element(By.CSS_SELECTOR, locators.continue_btn).click()
-    assert driver.current_url == 'https://www.saucedemo.com/checkout-step-two.html'
-    driver.find_element(By.CSS_SELECTOR, locators.finish_btn).click()
-    assert driver.find_element(By.CSS_SELECTOR, locators.complete_header).text == 'Thank you for your order!'
-    assert driver.find_element(By.CSS_SELECTOR, locators.complete_text).text == 'Your order has been dispatched, and will arrive just as fast as the pony can get there!'
-    driver.find_element(By.CSS_SELECTOR, locators.back_to_products).click()
-    assert driver.current_url == 'https://www.saucedemo.com/inventory.html'
+    browser.find_element(By.XPATH, locators.add_to_cart_red_tshirt).click()
+    assert browser.find_element(By.XPATH, locators.cart_badge).text == '1'
+    browser.find_element(By.CLASS_NAME, locators.cart_link).click()
+    assert browser.find_element(By.CLASS_NAME, locators.cart_quantity).text == '1'
+    browser.find_element(By.CSS_SELECTOR, locators.checkout).click()
+    assert browser.current_url == 'https://www.saucedemo.com/checkout-step-one.html'
+    browser.find_element(By.CSS_SELECTOR, locators.first_name_field).send_keys('Howard')
+    browser.find_element(By.CSS_SELECTOR, locators.last_name_field).send_keys('Lovecraft')
+    browser.find_element(By.CSS_SELECTOR, locators.postal_code).send_keys('322')
+    browser.find_element(By.CSS_SELECTOR, locators.continue_btn).click()
+    assert browser.current_url == 'https://www.saucedemo.com/checkout-step-two.html'
+    browser.find_element(By.CSS_SELECTOR, locators.finish_btn).click()
+    assert browser.find_element(By.CSS_SELECTOR, locators.complete_header).text == 'Thank you for your order!'
+    assert (browser.find_element(By.CSS_SELECTOR, locators.complete_text).text ==
+            'Your order has been dispatched, and will arrive just as fast as the pony can get there!')
+    browser.find_element(By.CSS_SELECTOR, locators.back_to_products).click()
+    assert browser.current_url == 'https://www.saucedemo.com/inventory.html'
 
 
 # Фильтр
 def test_checking_filters_from_a_to_z(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.CSS_SELECTOR, locators.product_sort_container).click()
-    driver.find_element(By.XPATH, locators.sort_a_to_z).click()
-    assert len(driver.find_elements(By.CSS_SELECTOR, locators.inventory_item)) == 6
-    elements = driver.find_elements(By.XPATH, locators.item_names)
+    browser.find_element(By.CSS_SELECTOR, locators.product_sort_container).click()
+    browser.find_element(By.XPATH, locators.sort_a_to_z).click()
+    assert len(browser.find_elements(By.CSS_SELECTOR, locators.inventory_item)) == 6
+    elements = browser.find_elements(By.XPATH, locators.item_names)
     lst = []
     for e in elements:
         lst.append(e.text)
@@ -128,12 +129,12 @@ def test_checking_filters_from_a_to_z(login):
 
 
 def test_checking_filters_from_z_to_a(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.CSS_SELECTOR, locators.product_sort_container).click()
-    driver.find_element(By.XPATH, locators.sort_z_to_a).click()
-    assert len(driver.find_elements(By.CSS_SELECTOR, locators.inventory_item)) == 6
-    elements = driver.find_elements(By.XPATH, locators.item_names)
+    browser.find_element(By.CSS_SELECTOR, locators.product_sort_container).click()
+    browser.find_element(By.XPATH, locators.sort_z_to_a).click()
+    assert len(browser.find_elements(By.CSS_SELECTOR, locators.inventory_item)) == 6
+    elements = browser.find_elements(By.XPATH, locators.item_names)
     lst = []
     for e in elements:
         lst.append(e.text)
@@ -141,12 +142,12 @@ def test_checking_filters_from_z_to_a(login):
 
 
 def test_checking_filters_from_low_to_high(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.CSS_SELECTOR, locators.product_sort_container).click()
-    driver.find_element(By.XPATH, locators.sort_low_to_high).click()
-    assert len(driver.find_elements(By.CSS_SELECTOR, locators.inventory_item)) == 6
-    elements = driver.find_elements(By.CSS_SELECTOR, locators.inventory_item_price)
+    browser.find_element(By.CSS_SELECTOR, locators.product_sort_container).click()
+    browser.find_element(By.XPATH, locators.sort_low_to_high).click()
+    assert len(browser.find_elements(By.CSS_SELECTOR, locators.inventory_item)) == 6
+    elements = browser.find_elements(By.CSS_SELECTOR, locators.inventory_item_price)
     lst = []
     for e in elements:
         lst.append(e.text)
@@ -154,12 +155,12 @@ def test_checking_filters_from_low_to_high(login):
 
 
 def test_checking_filters_from_high_to_low(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.CSS_SELECTOR, locators.product_sort_container).click()
-    driver.find_element(By.XPATH, locators.sort_high_to_low).click()
-    assert len(driver.find_elements(By.CSS_SELECTOR, locators.inventory_item)) == 6
-    elements = driver.find_elements(By.CSS_SELECTOR, locators.inventory_item_price)
+    browser.find_element(By.CSS_SELECTOR, locators.product_sort_container).click()
+    browser.find_element(By.XPATH, locators.sort_high_to_low).click()
+    assert len(browser.find_elements(By.CSS_SELECTOR, locators.inventory_item)) == 6
+    elements = browser.find_elements(By.CSS_SELECTOR, locators.inventory_item_price)
     lst = []
     for e in elements:
         lst.append(e.text)
@@ -168,58 +169,58 @@ def test_checking_filters_from_high_to_low(login):
 
 # Бургер меню
 def test_logout(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.CSS_SELECTOR, locators.burger_menu_btn).click()
-    driver.implicitly_wait(0.5)
-    driver.find_element(By.CSS_SELECTOR, locators.logout_link).click()
-    assert driver.current_url == 'https://www.saucedemo.com/'
+    browser.find_element(By.CSS_SELECTOR, locators.burger_menu_btn).click()
+    browser.implicitly_wait(0.5)
+    browser.find_element(By.CSS_SELECTOR, locators.logout_link).click()
+    assert browser.current_url == 'https://www.saucedemo.com/'
 
 
 def test_check_button_about(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.CSS_SELECTOR, locators.burger_menu_btn).click()
-    driver.implicitly_wait(0.5)
-    driver.find_element(By.CSS_SELECTOR, locators.about_link).click()
-    assert driver.find_element(By.CSS_SELECTOR, locators.body).text != '403 Forbidden', 'Страница недоступна: 404'
+    browser.find_element(By.CSS_SELECTOR, locators.burger_menu_btn).click()
+    browser.implicitly_wait(0.5)
+    browser.find_element(By.CSS_SELECTOR, locators.about_link).click()
+    assert browser.find_element(By.CSS_SELECTOR, locators.body).text != '403 Forbidden', 'Страница недоступна: 404'
 
 
 def test_check_button_reset_app_state(login):
-    driver = login
+    browser = login
 
-    driver.find_element(By.CSS_SELECTOR, locators.product_sort_container).click()
-    driver.find_element(By.XPATH, locators.sort_z_to_a).click()
-    assert len(driver.find_elements(By.CSS_SELECTOR, locators.inventory_item)) == 6
-    elements = driver.find_elements(By.XPATH, locators.item_names)
+    browser.find_element(By.CSS_SELECTOR, locators.product_sort_container).click()
+    browser.find_element(By.XPATH, locators.sort_z_to_a).click()
+    assert len(browser.find_elements(By.CSS_SELECTOR, locators.inventory_item)) == 6
+    elements = browser.find_elements(By.XPATH, locators.item_names)
     lst = []
     for e in elements:
         lst.append(e.text)
     assert lst == sorted(lst, reverse=True)
 
-    driver.find_element(By.CSS_SELECTOR, locators.burger_menu_btn).click()
-    driver.implicitly_wait(0.5)
-    driver.find_element(By.CSS_SELECTOR, locators.reset_link).click()
-    elements = driver.find_elements(By.XPATH, locators.item_names)
+    browser.find_element(By.CSS_SELECTOR, locators.burger_menu_btn).click()
+    browser.implicitly_wait(0.5)
+    browser.find_element(By.CSS_SELECTOR, locators.reset_link).click()
+    elements = browser.find_elements(By.XPATH, locators.item_names)
     lst = []
     for e in elements:
         lst.append(e.text)
     assert lst == sorted(lst)
 
-    driver.find_element(By.CSS_SELECTOR, locators.add_backpack).click()
-    driver.find_element(By.CSS_SELECTOR, locators.burger_menu_btn).click()
-    driver.implicitly_wait(0.5)
-    driver.find_element(By.CSS_SELECTOR, locators.reset_link).click()
+    browser.find_element(By.CSS_SELECTOR, locators.add_backpack).click()
+    browser.find_element(By.CSS_SELECTOR, locators.burger_menu_btn).click()
+    browser.implicitly_wait(0.5)
+    browser.find_element(By.CSS_SELECTOR, locators.reset_link).click()
     with pytest.raises(NoSuchElementException):
-        driver.find_element(By.XPATH, locators.cart_badge)
+        browser.find_element(By.XPATH, locators.cart_badge)
         pytest.fail('Не должно быть товаров в корзине')
 
 
 # Чек бокс
 def test_check_box():
-    driver = webdriver.Chrome()
-    driver.get('https://victoretc.github.io/webelements_information/')
-    driver.find_element(By.CSS_SELECTOR, locators.check_login).send_keys('1')
-    driver.find_element(By.CSS_SELECTOR, locators.check_password).send_keys('1')
+    browser = webdriver.Chrome()
+    browser.get('https://victoretc.github.io/webelements_information/')
+    browser.find_element(By.CSS_SELECTOR, locators.check_login).send_keys('1')
+    browser.find_element(By.CSS_SELECTOR, locators.check_password).send_keys('1')
 
-    assert driver.find_element(By.CSS_SELECTOR, locators.check_reg_button).get_attribute('disabled')
+    assert browser.find_element(By.CSS_SELECTOR, locators.check_reg_button).get_attribute('disabled')
